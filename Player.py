16367e4951c3dcc8ef9810c6210f5dc61b25bb6c
Alex Liao <ahliao@umich.edu>
2014-01-11 00:35:51 -0500

    Added some edge cases

--- Player.py
+++ Player.py
@@ -11,9 +11,24 @@ class Player:
 		pass
 
 	def playRequest(self, comm):
-		comm.playCard(comm.hand[1])
+		if comm.card == None: # we go first
+			if len(comm.hand) == 5:
+				comm.playCard(comm.hand[2])
+			else:
+				comm.playCard(comm.hand[0])
+		else:
+			if comm.card >= max(comm.hand):
+				# play the lowest card
+				comm.playCard(comm.hand[len(comm.hand)-1])
+			else:
+				# play the lowest card that can win
+				for x in reversed(range(len(comm.hand))):
+					if comm.hand[x] > comm.card:
+						comm.playCard(comm.hand[x])
+				
 		
 	def challenged(self, comm):
+
 		comm.acceptChallenge()
 
 	def result(self, comm):
